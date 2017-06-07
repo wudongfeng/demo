3c10c7b05b9f3c8087010240934b1950785689e5
wudongfeng007 <wudongfeng@baidu.com>
2017-06-07 13:49:11 +0800

    增加了一个测试文件,主要用来测试一些基本的内容

---
+++ src/test/java/com/baidu/DemoForTest.java
@@ -0,0 +1,29 @@
+package com.baidu;
+
+import com.baidu.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wudongfeng on 17/6/7.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoForTest {
+    @Test
+    public void testHashMap(){
+        Map<String,User> usermap=new HashMap<>();
+        User user=new User();
+        user.setUsername("wdf");
+        user.setPassword("1989");
+        usermap.put(user.getUsername(),user);
+        System.out.println("the output:"+Map.Entry.class);
+
+    }
+
+}
